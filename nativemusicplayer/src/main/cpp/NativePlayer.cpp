773701ea22e7b2cb43df59634b97d0d307c4c652
dipak140 <dipak@reconv.in>
2024-10-18 10:05:08 +0530

    added a new moduele

---
+++ nativemusicplayer/src/main/cpp/NativePlayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dipak Sisodiya on 18/10/24.
+//
+
+#include "NativePlayer.h"
---
+++ nativemusicplayer/src/main/cpp/NativePlayer.h
@@ -0,0 +1,14 @@
+//
+// Created by Dipak Sisodiya on 18/10/24.
+//
+
+#ifndef OBOE_MP3_PLAYER_NATIVEPLAYER_H
+#define OBOE_MP3_PLAYER_NATIVEPLAYER_H
+
+
+class NativePlayer {
+
+};
+
+
+#endif //OBOE_MP3_PLAYER_NATIVEPLAYER_H
